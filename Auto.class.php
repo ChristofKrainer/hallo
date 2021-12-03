5e5e51dc25d54558d538bbbdd0c4dc2792d0e8fa
ChristofKrainer <95438010+ChristofKrainer@users.noreply.github.com>
2021-12-03 09:15:47 +0100

    Update Auto.class.php

--- Auto.class.php
+++ Auto.class.php
@@ -1,19 +1,1 @@
-<?php
-abstract class Auto {
-    //ein Attribut, eine Eigenschaft
-    private $geschwindigkeit = 0;
-    protected function setGeschwindigkeit($geschwindigkeit){
-        $this->geschwindigkeit=$geschwindigkeit;
-    }
-    protected function getGeschwindigkeit(){
-        return $this->geschwindigkeit;
-    }
-    
 
-    //zum Bremsen
-    
-    public abstract function beschleunigen($aenderung);
-
-}
-
-?>
