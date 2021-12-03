a4dd056f08023f53d4561f2e3356768fac37a54c
ChristofKrainer <krainer.christof@hakspittal.at>
2021-12-03 09:11:19 +0100

    neue Datei

---
+++ Auto.class.php
@@ -0,0 +1,20 @@
+<?php
+abstract class Auto {
+    //ein Attribut, eine Eigenschaft
+    private $geschwindigkeit = 0;
+    protected function setGeschwindigkeit($geschwindigkeit){
+        $this->geschwindigkeit=$geschwindigkeit;
+    }
+    protected function getGeschwindigkeit(){
+        return $this->geschwindigkeit;
+    }
+    
+
+    //zum Bremsen
+    public abstract function bremsen($aenderung);
+    //zum Gasgeben
+    public abstract function beschleunigen($aenderung);
+
+}
+
+?>
