3e3181684166cf9864c144376deebc73adcde756
ChristofKrainer <krainer.christof@hakspittal.at>
2021-12-03 09:28:21 +0100

    Änderung

---
+++ Lastkraftwagen.class.php
@@ -0,0 +1,31 @@
+<?php
+require_once("Auto.class.php");
+class Lastkraftwagen extends Auto{
+    private $hoechstgeschwindigkeit = 120;
+    public function beschleunigen($aenderung) {
+        if ($this->getGeschwindigkeit() + $aenderung < $this->hoechstgeschwindigkeit) {
+            $this->setGeschwindigkeit(
+                $this->getGeschwindigkeit() + $aenderung);
+            echo "Die aktuelle Geschwindigkeit des LKW beträgt "
+                .$this->getGeschwindigkeit() ."<br />";
+        }
+        
+        else{
+            $this->setGeschwindigkeit($this->hoechstgeschwindigkeit);
+            echo "Die maximale Geschwindigkeit des LKW beträgt "
+                .$this->hoechstgeschwindigkeit ."<br /> ";
+        }
+    }
+    //zum Bremsen
+    public function bremsen($aenderung) {
+        if ($this->getGeschwindigkeit() - $aenderung < 0) {
+            $this->setGeschwindigkeit(0);
+        }
+        else {
+            $this->setGeschwindigkeit($this->getGeschwindigkeit() - $aenderung);
+        }
+        echo "Die aktuelle Geschwindigkeit des LKW beträgt "
+            .$this->getGeschwindigkeit() ."<br />";
+    }
+}
+?>
